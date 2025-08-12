effe12489a32b8981c17e51b0308afa15a02a336
William Hicklin <venturehicklin@gmail.com>
2025-08-12 15:27:59 +0100

    Disabled some nouveau features to smooth operation with K600

--- nixos/graphics.nix
+++ nixos/graphics.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  hardware.firmware = with pkgs; [ linux-firmware ];
+
   # Explicitly use nouveau
   services.xserver.videoDrivers = [ "nouveau" ];
   
@@ -15,4 +17,19 @@
     mesa-demos
     iotop
   ];
+
+  # Setting for K600 with nouveau
+  # Minimize crashes - disable acceleration features
+  boot.kernelParams = [
+    "nouveau.modeset=1"
+    "nouveau.runpm=0"
+    "nouveau.noaccel=0"  # Keep basic acceleration
+    "nouveau.vram_pushbuf=0"
+  ];
+
+  # Disable problematic video acceleration
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "";  # Disable VAAPI
+    VDPAU_DRIVER = "";       # Disable VDPAU
+  };
 }
